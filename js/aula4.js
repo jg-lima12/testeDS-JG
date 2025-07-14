f3660461e8e2f2ad6ddd94db6d734f57809861d1
João Almeida <mr.johnplay0108@gmail.com>
2025-07-14 17:35:36 -0400

    ÇVO9SRPOGUP9ER

---
+++ js/aula4.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+/*
+&& e
+|| ou
+! nao
+*/
+
+let n1,n2,n3,n4;
+
+n1 = 10;
+n2 = 15;
+n3 = 5;
+n4 = 2;
+
+console.log((n1>n2)||(n3>n4))
---
+++ js/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+function cal(){
+    nome = 'Joao'
+    let nome
+    return nome
+}
+let teste
+teste = 'oi';
+
+console.log(teste);
+
+
+
+
+
+
+
+
+
+
+
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question("Digite um valor: ", (resposta) => {
+//   console.log(`Você digitou: ${resposta}`);
+//   rl.close();
+// });
